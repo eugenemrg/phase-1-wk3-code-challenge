ea27abd0cd51061c1c27481a02b02ab18b0daf0a
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-02 09:10:22 +0300

    Added functionality for film item click, click triggers populating of film details/summary section with details of the clicked film

--- js/script.js
+++ js/script.js
@@ -9,6 +9,9 @@ fetch('http://localhost:3008/films') //http://localhost:3008/films
 
             let item = document.createElement('li')
             item.className = 'film item'
+            item.addEventListener('click', (e)=>{
+                populateSummary(filmItem)
+            })
 
             let poster = document.createElement('img')
             poster.className = 'poster'
@@ -40,3 +43,41 @@ fetch('http://localhost:3008/films') //http://localhost:3008/films
         });
 
     })
+
+function populateSummary(filmItemData) {
+    const title = filmItemData.title
+    const poster = filmItemData.poster
+    const description = filmItemData.description
+    const runtime = filmItemData.runtime
+    const showtime = filmItemData.showtime
+    const availableTickets = filmItemData.capacity - filmItemData.tickets_sold
+    const availableTicketsMessage = (availableTickets === 0) ? 'sold out' : 'buy ticket '
+
+    let filmTitle = document.querySelector('.film-title')
+    filmTitle.innerText = title
+
+    let filmPoster = document.querySelector('.film-poster')
+    filmPoster.src = poster
+
+    let filmDescription = document.querySelector('.film-synopsis')
+    filmDescription.innerText = description
+
+    let filmShowtime = document.querySelector('.film-showtime')
+    filmShowtime.innerText = showtime
+
+    let filmRuntime = document.querySelector('.film-runtime')
+    filmRuntime.innerText = runtime
+
+    let filmTickets = document.querySelector('.film-tickets')
+    filmTickets.innerText = availableTickets
+
+    let ticketButton = document.querySelector('.purchase')
+    ticketButton.innerText = availableTicketsMessage
+
+    if(availableTickets > 0) {
+        // code to add right chevron arrow on ticket button shown on button if tickets aren't sold out
+        let icon = document.createElement('i')
+        icon.className = 'fa-solid fa-chevron-right'
+        ticketButton.appendChild(icon)
+    }
+}
